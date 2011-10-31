6de955786a63e1e386caef00611d0cd79b0ca9c8
Cyril <cyril78@gmail.com>
2011-10-31 21:13:58 +0100

    Squelette de l'application

---
+++ android/src/fr/sosmessagedecarte/SosActivity.java
@@ -0,0 +1,29 @@
+package fr.sosmessagedecarte;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class SosActivity extends Activity {
+	/** Called when the activity is first created. */
+
+	private TextView text;
+	private Button myButton;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		// text = (TextView) findViewById(R.id.text);
+		// myButton = (Button) findViewById(R.id.myButton);
+
+		// myButton.setOnClickListener(new View.OnClickListener() {
+		// public void onClick(View v) {
+		// text.setText("hohoho");
+		// }
+		// });
+
+	}
+}
