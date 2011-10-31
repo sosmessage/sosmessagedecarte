dde3efb40bf4985f6bccb54517646c6e1911a2c2
Arnaud Kervern <arnaud.k@gmail.com>
2011-10-31 11:35:49 +0100

    Use a unique view to handle iPhone and iPad

--- server/server.rb
+++ server/server.rb
@@ -5,5 +5,5 @@ require "json"
 messages = ["Mon premier texte de bienvenu", "Mon deuxieme texte un peu funny", "Mon troisieme texte ou l'on se marre"]
 
 get "/v1/messages" do
-  ["message" => messages[rand(3)]].to_json
+  {"message" => messages[rand(3)]}.to_json
 end
