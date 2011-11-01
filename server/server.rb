3b063664c4746677659cb799b98493051abebf85
Arnaud Kervern <arnaud.k@gmail.com>
2011-11-01 11:41:33 +0100

    Add an announce entry into the json result, that may be empty.

--- server/server.rb
+++ server/server.rb
@@ -5,5 +5,9 @@ require "json"
 messages = ["Mon premier texte de bienvenu", "Mon deuxieme texte un peu funny", "Mon troisieme texte ou l'on se marre"]
 
 get "/v1/messages" do
-  {"message" => messages[rand(3)]}.to_json
+  response = {"message" => messages[rand(messages.size)]}
+  if (rand(2) % 2 == 0) 
+    response["announce"] = {"content" => "Do not forget to give your feedback !"}
+  end
+  response.to_json
 end
