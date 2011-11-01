f8a027d042949e4163c4a637552aadf6cd60c708
Arnaud Kervern <arnaud.k@gmail.com>
2011-11-01 21:50:10 +0100

    Split server into two separate file and add admin skeleton

--- server/spec/spec_helper.rb
+++ server/spec/spec_helper.rb
@@ -1,10 +1,10 @@
-require File.join(File.dirname(__FILE__), '..', 'server.rb')
-
 require 'rubygems'
 require 'sinatra'
 require 'json'
 require 'rack'
 require 'rack/test'
+
+require File.join(File.dirname(__FILE__), '..', 'server_v1')
 
 # set test environment
 set :environment, :test
