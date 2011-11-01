7258a16eb7af6800f0ecf5d2dd18883d88162be1
Cyril <cyril78@gmail.com>
2011-11-01 21:44:09 +0100

    fix logo defauts and add mock default messages.

--- android/gen/fr/sosmessagedecarte/R.java
+++ android/gen/fr/sosmessagedecarte/R.java
@@ -39,8 +39,11 @@ public final class R {
         public static final int remerciement=0x7f030004;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f040004;
+        public static final int mock_anniv=0x7f040003;
+        public static final int mock_mariage=0x7f040000;
+        public static final int mock_pot=0x7f040002;
+        public static final int mock_remerciement=0x7f040001;
     }
     public static final class style {
         /**  item name="android:windowBackground">@drawable/handmadepaper</item
