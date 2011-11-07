2092f61ccd210784dc1eab55949b9e0dafa2c9d6
Arnaud Kervern <akervern@nuxeo.com>
2011-11-07 17:49:04 +0100

    Add a dummy message text - #15

--- iOS/sosmessage/sosmessage/SMDetailViewController.m
+++ iOS/sosmessage/sosmessage/SMDetailViewController.m
@@ -10,9 +10,11 @@
 #import <CoreText/CoreText.h>
 
 @implementation SMDetailViewController
+@synthesize titleImage;
 @synthesize messageImage;
 
 float baseHue;
+NSString* tmpMessage = @"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque commodo aliquam semper. Donec volutpat, metus in vulputate mattis, massa massa porttitor nisl, non aliquam nibh elit a enim. Duis ac enim turpis, ut blandit leo. Quisque vulputate blandit dapibus. Suspendisse pretium, felis vel aliquam vestibulum, magna elit eleifend dolor, molestie fermentum lectus massa ut elit. Cras eget neque mauris, ut consequat augue. Donec vel facilisis eros.";
 
 - (id)initWithHue:(float)hue category:(NSString*)category {
     self = [super initWithNibName:@"SMDetailViewController" bundle:nil];
@@ -40,14 +42,14 @@ float baseHue;
 
 -(void)viewWillAppear:(BOOL)animated 
 {
-    UIGraphicsBeginImageContext(self.messageImage.bounds.size);
+    UIGraphicsBeginImageContext(self.titleImage.bounds.size);
     CGContextRef context = UIGraphicsGetCurrentContext();
     
-    CGAffineTransform flipTransform = CGAffineTransformMake( 1, 0, 0, -1, 0, self.messageImage.frame.size.height);
+    CGAffineTransform flipTransform = CGAffineTransformMake( 1, 0, 0, -1, 0, self.titleImage.frame.size.height);
     CGContextConcatCTM(context, flipTransform);
     
     CGMutablePathRef path = CGPathCreateMutable();
-    CGPathAddRect(path, NULL, self.messageImage.bounds);
+    CGPathAddRect(path, NULL, self.titleImage.bounds);
     
     NSString *header = @"sosmessagedecarte\ndevisite";
     NSInteger _stringLength=[header length];
@@ -90,7 +92,9 @@ float baseHue;
     UIImage* result = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
     
-    self.messageImage.image = result;
+    self.titleImage.image = result;
+    
+    [self fetchAMessage];
     
     [super viewWillAppear:animated];
 }
@@ -103,6 +107,7 @@ float baseHue;
 
 - (void)viewDidUnload
 {
+    [self setTitleImage:nil];
     [self setMessageImage:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
@@ -116,7 +121,61 @@ float baseHue;
 }
 
 - (void)dealloc {
+    [titleImage release];
     [messageImage release];
     [super dealloc];
 }
+
+#pragma mark Custom methods
+
+-(void)fetchAMessage {
+    UIGraphicsBeginImageContext(self.messageImage.bounds.size);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    
+    CGAffineTransform flipTransform = CGAffineTransformMake( 1, 0, 0, -1, 0, self.messageImage.frame.size.height);
+    CGContextConcatCTM(context, flipTransform);
+    
+    CGMutablePathRef path = CGPathCreateMutable();
+    CGPathAddRect(path, NULL, self.messageImage.bounds);
+    NSInteger _stringLength=[tmpMessage length];
+    
+    CFMutableAttributedStringRef attrString = CFAttributedStringCreateMutable(kCFAllocatorDefault, 0);
+    CFAttributedStringReplaceString (attrString,CFRangeMake(0, 0), (CFStringRef)tmpMessage);
+    
+    CGColorRef _hue=[UIColor colorWithHue:baseHue saturation:0.9 brightness:0.7 alpha:1].CGColor;
+    CFAttributedStringSetAttribute(attrString, CFRangeMake(0, 1),kCTForegroundColorAttributeName, _hue);
+
+    CTFontRef font = CTFontCreateWithName((CFStringRef)@"Helvetica", 16, nil);
+    CFAttributedStringSetAttribute(attrString,CFRangeMake(0, _stringLength),kCTFontAttributeName,font);
+    
+    /** TODO find a way to reduce the leading
+    font = CTFontCreateWithName((CFStringRef)@"Helvetica", 35, nil);
+    CFAttributedStringSetAttribute(attrString,CFRangeMake(0, 1),kCTFontAttributeName,font);
+     */
+    
+    CGFloat indent = 25.0f;
+    CTTextAlignment alignement = kCTJustifiedTextAlignment;
+    CTParagraphStyleSetting settings[] = {kCTParagraphStyleSpecifierFirstLineHeadIndent, sizeof(indent), &indent, kCTParagraphStyleSpecifierAlignment, sizeof(alignement), &alignement};
+    CTParagraphStyleRef paragraph = CTParagraphStyleCreate(settings, sizeof(settings) / sizeof(settings[0]));
+    CFAttributedStringSetAttribute(attrString, CFRangeMake(0, _stringLength), kCTParagraphStyleAttributeName, paragraph);
+    CFRelease(paragraph);
+    
+    // Create the framesetter with the attributed string.
+    CTFramesetterRef framesetter =
+    CTFramesetterCreateWithAttributedString(attrString);
+    CFRelease(attrString);
+    
+    // Create the frame and draw it into the graphics context
+    CTFrameRef frame = CTFramesetterCreateFrame(framesetter, CFRangeMake(0, 0), path, NULL);
+    CFRelease(framesetter);
+    CTFrameDraw(frame, context);
+    CFRelease(frame);
+    CFRelease(path);
+    
+    UIImage* result = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    self.messageImage.image = result;
+}
+
 @end
